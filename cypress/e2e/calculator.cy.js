479c2558ddc1d9f7c1284d00d5ff490702ee0f8c
Charlito <cruizfreak@hotmail.com>
2022-07-11 09:44:59 -0600

    se agrego la baseURL, la operacion por parametro y el switch condition

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -2,39 +2,65 @@
 import calculadoraPage from '../page-objects/calculadora.page'
 import datos from '../fixtures/datos.json'
 
-let numeroA = datos.numero1, numeroB = datos.numero2, resultado_operacion = 0;
+let numeroA = datos.numero1, numeroB = datos.numero2, operacion = datos.operacion, resultado_operacion;;
 const calculadora = new calculadoraPage();
 
 beforeEach("Ingresar a la pagina", ()=>{
-  cy.visit('https://testsheepnz.github.io/BasicCalculator.html')
+  cy.visit('/BasicCalculator.html');
+  calculadora.ingresarNumeros(numeroA, numeroB); 
 });
-describe.only('Hacer operaciones en la calculadora', () => {
+
+afterEach("Validar el resultado", ()=>{
+  calculadora.getResultado().should('have.value', resultado_operacion);
+});
+
+describe('Hacer operaciones en la calculadora', () => {
+  it('Hacer las operaciones completas', () => {
+    calculadora.seleccionarOperacion(operacion)
+    calculadora.clickBtnCalcular();
+    switch (operacion) {
+      case "Add":
+        resultado_operacion = numeroA + numeroB;
+        break;
+      case "Subtract":
+        resultado_operacion = numeroA - numeroB;
+        break;
+      case "Multiply":
+        resultado_operacion = numeroA * numeroB;
+        break;      
+      case "Divide":
+        resultado_operacion = numeroA / numeroB;
+        break;
+      case "Concatenate":
+          resultado_operacion = numeroA.toString() + numeroB.toString();
+          break;
+      default:alert("Operacion no valida");
+        break;
+    }
+  })
   it('Sumar dos numeros', () => {
-    calculadora.ingresarNumeros(numeroA, numeroB); 
     calculadora.seleccionarOperacion('Add')
     calculadora.clickBtnCalcular();
     resultado_operacion = numeroA + numeroB;
-    calculadora.getResultado().should('have.value', resultado_operacion);
   })
   it('Restar dos numeros', () => {
-    calculadora.ingresarNumeros(numeroA, numeroB); 
     calculadora.seleccionarOperacion('Subtract')
     calculadora.clickBtnCalcular();
     resultado_operacion = numeroA - numeroB;
-    calculadora.getResultado().should('have.value', resultado_operacion);
   })
   it('Multiplicar dos numeros', () => {
-    calculadora.ingresarNumeros(numeroA, numeroB); 
     calculadora.seleccionarOperacion('Multiply')
     calculadora.clickBtnCalcular();
     resultado_operacion = numeroA * numeroB;
-    calculadora.getResultado().should('have.value', resultado_operacion);
   })
   it('Dividir dos numeros', () => {
-    calculadora.ingresarNumeros(numeroA, numeroB); 
     calculadora.seleccionarOperacion('Divide')
     calculadora.clickBtnCalcular();
     resultado_operacion = numeroA / numeroB;
-    calculadora.getResultado().should('have.value', resultado_operacion);
+  })
+  it('Concatenar dos numeros', () => {
+    calculadora.seleccionarOperacion('Concatenate')
+    calculadora.clickBtnCalcular();
+    resultado_operacion = numeroA.toString() + numeroB.toString();
   })
 })
